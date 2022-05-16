9c42a8bf9030916e7a33a86e99176f867d4d2491
NickSchenck <yoyo4games@yahoo.com>
2022-05-15 23:25:09 -0500

    functions for communicating with the user

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,10 @@
-window.alert("This is an alert that JavaScript is running!")
+var playerName = window.prompt("What is your robot's name?");
+//Note the lack of quotation marks around playerName
+console.log(playerName);
+console.log("This logs a string, for leaving messages");
+console.log(10 + 10); //this will do math
+console.log("Our robot's name is " + playerName);
+function fight(){
+    window.alert("The fight has begun!");
+}
+//fight();
